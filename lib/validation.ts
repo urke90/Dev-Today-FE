ac922479ccff10665e0a0d387f29373ea70c53e5
MarkoTodorovic <markec026@yahoo.com>
2024-04-16 23:53:33 +0200

    add login and register page

---
+++ lib/validation.ts
@@ -0,0 +1,11 @@
+import * as z from "zod";
+export const signInSchema = z.object({
+  name: z.string().min(3).max(20),
+  email: z.string().email(),
+  password: z.string().min(6),
+});
+
+export const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+});
