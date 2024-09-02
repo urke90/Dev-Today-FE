4816a904d5e3fd844d43f8849f02f407ef9827f1
urke90 <urosbijelic90@gmail.com>
2024-09-02 15:16:35 +0200

    remove app/api/user/route.ts file with some custom unnecessary logic for middleware.ts

--- app/api/user/route.ts
+++
@@ -1,8 +0,0 @@
-import { getServerSession } from 'next-auth';
-
-import { authOptions } from '@/lib/auth-options';
-
-export async function GET(request: Request) {
-  const session = await getServerSession(authOptions);
-  return Response.json(session);
-}
