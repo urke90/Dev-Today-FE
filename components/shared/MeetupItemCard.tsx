6381309ae3c4a49dab2da6b36c1fb36bf42617a6
urke90 <urosbijelic90@gmail.com>
2024-09-05 20:30:43 +0200

    update main Card Items, replace Next Links with Next router

--- components/shared/MeetupItemCard.tsx
+++ components/shared/MeetupItemCard.tsx
@@ -4,7 +4,7 @@ import BadgeItem from './BadgeItem';
 
 import parse from 'html-react-parser';
 import Image from 'next/image';
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 import type { ITag } from '@/types/content';
 import { parseDate } from '@/utils/format';
@@ -32,46 +32,45 @@ const MeetupItemCard: React.FC<IMeetupItemCardProps> = ({
 }) => {
   const shortenedDate = meetupDate ? parseDate(meetupDate) : 'TBD';
   const [month, day] = shortenedDate.split(' ');
+  const router = useRouter();
 
   return (
-    <li>
-      <Link
-        href={'/content/' + id}
-        className="bg-light100__dark800 flex flex-col gap-2.5 rounded-[10px] px-3.5 py-5 shadow-card"
-      >
-        <div className="flex-between flex">
-          <div className="flex items-center gap-2 md:gap-4">
-            <div className="relative size-[56px] shrink-0 md:size-[72px]">
-              <Image
-                src={coverImage || '/assets/icons/image-preview.svg'}
-                alt="meetup"
-                fill
-                className="size-[56px] md:size-[72px]"
-              />
-            </div>
-            <div>
-              <p className="lg:p1-bold p3-bold">{title}</p>
-              <p className="lg:p3-regular subtitle-normal">{address}</p>
-            </div>
+    <li
+      onClick={() => router.push('/content/' + id)}
+      className="bg-light100__dark800 shadow-card flex flex-col gap-2.5 rounded-[10px] px-3.5 py-5"
+    >
+      <div className="flex-between flex">
+        <div className="flex items-center gap-2 md:gap-4">
+          <div className="relative size-[56px] shrink-0 md:size-[72px]">
+            <Image
+              src={coverImage || '/assets/icons/image-preview.svg'}
+              alt="meetup"
+              fill
+              className="size-[56px] md:size-[72px]"
+            />
           </div>
-          <div className="flex-center bg-light200__dark700 h-[56px] w-[38px] shrink-0 flex-col rounded-[6px] px-2.5 py-[5px] md:h-[58px] md:w-[54px]">
-            <span className="subtitle-normal md:p4-regular uppercase text-black-800 dark:text-white-200">
-              {month}
-            </span>
-            <span className="p2-bold md:d2-bold !text-primary-500">{day}</span>
+          <div>
+            <p className="lg:p1-bold p3-bold">{title}</p>
+            <p className="lg:p3-regular subtitle-normal">{address}</p>
           </div>
         </div>
-        <div className="p4-regular md:p3-regular line-clamp-2">
-          {parse(description)}
+        <div className="flex-center bg-light200__dark700 h-[56px] w-[38px] shrink-0 flex-col rounded-[6px] px-2.5 py-[5px] md:h-[58px] md:w-[54px]">
+          <span className="subtitle-normal md:p4-regular text-black-800 dark:text-white-200 uppercase">
+            {month}
+          </span>
+          <span className="p2-bold md:d2-bold !text-primary-500">{day}</span>
         </div>
-        {tags?.length > 0 ? (
-          <ul className="flex flex-wrap gap-2.5">
-            {tags.map(({ id, title }) => (
-              <BadgeItem key={id} title={title} />
-            ))}
-          </ul>
-        ) : null}
-      </Link>
+      </div>
+      <div className="p4-regular md:p3-regular line-clamp-2">
+        {parse(description)}
+      </div>
+      {tags?.length > 0 ? (
+        <ul className="flex flex-wrap gap-2.5">
+          {tags.map(({ id, title }) => (
+            <BadgeItem key={id} title={title} />
+          ))}
+        </ul>
+      ) : null}
     </li>
   );
 };
--- components/shared/PostItemCard.tsx
+++ components/shared/PostItemCard.tsx
@@ -7,7 +7,7 @@ import { Button } from '../ui/button';
 
 import parse from 'html-react-parser';
 import Image from 'next/image';
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 import type { ITag } from '@/types/content';
 import { calculateTimeAgo, formatNumberWithCommas } from '@/utils/format';
@@ -54,91 +54,87 @@ const PostItemCard: React.FC<IPostItemCardProps> = ({
   handleDislikeContent,
   author,
 }) => {
-  console.log('isLiked', isLiked);
+  const router = useRouter();
 
   return (
-    <li>
-      <Link
-        href={'/content/' + id}
-        className="bg-light100__dark800 shadow-card flex gap-4 rounded-2xl p-4 md:items-center md:p-5"
-      >
-        <Image
-          src={coverImage || '/assets/icons/image-preview.svg'}
-          width={165}
-          height={165}
-          alt={title}
-          className="size-[165px] rounded-2xl object-cover max-md:hidden"
-        />
-        {/* RIGHT PART OF THE POST */}
-        <div className="flex flex-1 flex-col gap-4">
-          <div className="mb-4 flex justify-between gap-2">
-            <div className="flex gap-2">
-              <Image
-                src={coverImage || '/assets/icons/image-preview.svg'}
-                width={50}
-                height={50}
-                alt={title}
-                className="rounded-lg object-cover md:hidden"
-              />
-              <div>
-                <p className="p1-bold mb-2">{title}</p>
-                <div className="p3-regular line-clamp-1">
-                  {parse(description)}
-                </div>
+    <li
+      className="bg-light100__dark800 shadow-card flex gap-4 rounded-2xl p-4 md:items-center md:p-5"
+      onClick={() => router.push('/content/' + id)}
+    >
+      <Image
+        src={coverImage || '/assets/icons/image-preview.svg'}
+        width={165}
+        height={165}
+        alt={title}
+        className="size-[165px] rounded-2xl object-cover max-md:hidden"
+      />
+      {/* RIGHT PART OF THE POST */}
+      <div className="flex flex-1 flex-col gap-4">
+        <div className="mb-4 flex justify-between gap-2">
+          <div className="flex gap-2">
+            <Image
+              src={coverImage || '/assets/icons/image-preview.svg'}
+              width={50}
+              height={50}
+              alt={title}
+              className="rounded-lg object-cover md:hidden"
+            />
+            <div>
+              <p className="p1-bold mb-2">{title}</p>
+              <div className="p3-regular line-clamp-1">
+                {parse(description)}
               </div>
             </div>
-            <Button
-              type="button"
-              className="flex-center like-btn-scale-hover bg-white-200 dark:bg-black-700 size-[30px] shrink-0 rounded-full"
-              onClick={(e) =>
-                isLiked ? handleDislikeContent(e, id) : handleLikeContent(e, id)
-              }
-            >
-              <HeartIcon
-                className={isLiked ? 'text-primary-500' : 'text-white-300'}
-              />
-            </Button>
           </div>
-          {tags?.length > 0 ? (
-            <ul className="flex flex-wrap gap-2.5">
-              {tags.map(({ id, title }) => (
-                <BadgeItem key={id} title={title} />
-              ))}
-            </ul>
-          ) : null}
-          <div className="flex-between flex-wrap gap-5">
-            <div className="flex">
-              <div className="flex-center bg-white-600 mr-2.5 size-[40px] rounded-full">
-                <Image
-                  src={
-                    author.avatarImg || '/assets/images/avatars/avatar-1.svg'
-                  }
-                  width={28}
-                  height={28}
-                  alt="avatar"
-                  className="size-[28px] rounded-full"
-                />
-              </div>
-              <div>
-                <p className="p3-bold">{author.userName}</p>
-                <p className="subtitle-normal">{calculateTimeAgo(createdAt)}</p>
-              </div>
+          <Button
+            type="button"
+            className="flex-center like-btn-scale-hover bg-white-200 dark:bg-black-700 size-[30px] shrink-0 rounded-full"
+            onClick={(e) =>
+              isLiked ? handleDislikeContent(e, id) : handleLikeContent(e, id)
+            }
+          >
+            <HeartIcon
+              className={isLiked ? 'text-primary-500' : 'text-white-300'}
+            />
+          </Button>
+        </div>
+        {tags?.length > 0 ? (
+          <ul className="flex flex-wrap gap-2.5">
+            {tags.map(({ id, title }) => (
+              <BadgeItem key={id} title={title} />
+            ))}
+          </ul>
+        ) : null}
+        <div className="flex-between flex-wrap gap-5">
+          <div className="flex">
+            <div className="flex-center bg-white-600 mr-2.5 size-[40px] rounded-full">
+              <Image
+                src={author.avatarImg || '/assets/images/avatars/avatar-1.svg'}
+                width={28}
+                height={28}
+                alt="avatar"
+                className="size-[28px] rounded-full"
+              />
             </div>
-            <div className="text-white-400 dark:text-white-300 flex gap-[30px]">
-              <span className="p3-regular">
-                {formatNumberWithCommas(viewsCount)} Views
-              </span>
-              <span className="p3-regular">
-                {formatNumberWithCommas(likesCount)} Likes
-              </span>
-              <span className="p3-regular">
-                {formatNumberWithCommas(commentsCount)} comments
-              </span>
+            <div>
+              <p className="p3-bold">{author.userName}</p>
+              <p className="subtitle-normal">{calculateTimeAgo(createdAt)}</p>
             </div>
+          </div>
+          <div className="text-white-400 dark:text-white-300 flex gap-[30px]">
+            <span className="p3-regular">
+              {formatNumberWithCommas(viewsCount)} Views
+            </span>
+            <span className="p3-regular">
+              {formatNumberWithCommas(likesCount)} Likes
+            </span>
+            <span className="p3-regular">
+              {formatNumberWithCommas(commentsCount)} comments
+            </span>
           </div>
         </div>
-        {/* RIGHT PART OF THE POST */}
-      </Link>
+      </div>
+      {/* RIGHT PART OF THE POST */}
     </li>
   );
 };
